aa0c18842426196f21b57c60f45d68701aa0f404
Unblockerfire <145611531+Unblockerfire@users.noreply.github.com>
2025-11-21 17:45:35 -0700

    Initial Commit

---
+++ PeachyCleaner/PeachyCleanerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeachyCleanerApp.swift
+//  PeachyCleaner
+//
+//  Created by Carson Livezey on 11/21/25.
+//
+
+import SwiftUI
+
+@main
+struct PeachyCleanerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
